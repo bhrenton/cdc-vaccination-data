33238b8cd62bf9f61515f13ae4899b3a010dec65
bhrenton <78880483+bhrenton@users.noreply.github.com>
2021-04-06 00:17:36 -0400

    Add files via upload

---
+++ scrapedistributiondata.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+distribution_data <- read_csv("https://www.cdc.gov/coronavirus/2019-ncov/json/covid-vaccines-data.csv")
+write.csv(distribution_data, 'distributiondata.csv',row.names = FALSE)

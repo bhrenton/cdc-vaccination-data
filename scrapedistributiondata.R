f7cbfde2e53fa81cc62807bc1af43392ed89baa9
bhrenton <78880483+bhrenton@users.noreply.github.com>
2021-04-06 00:34:37 -0400

    Delete scrapedistributiondata.R

--- scrapedistributiondata.R
+++
@@ -1,3 +0,0 @@
-library(tidyverse)
-distribution_data <- read_csv("https://www.cdc.gov/coronavirus/2019-ncov/json/covid-vaccines-data.csv")
-write.csv(distribution_data, 'distributiondata.csv',row.names = FALSE)

70307ced93ab127a606818a06aed16a5c1383775
bhrenton <78880483+bhrenton@users.noreply.github.com>
2021-05-05 01:43:23 -0400

    Update make_timeseries.py

--- timeseries/make_timeseries.py
+++ timeseries/make_timeseries.py
@@ -1,3 +1,4 @@
+
 import json
 import git
 import csv
@@ -12,18 +13,17 @@ if sys.version_info < MIN_PYTHON:
 
 
 @click.command()
-@click.argument('filename')
-@click.argument('json_key')
 @click.option('--strip-duplicate-days/--no-strip-duplicate-days', default=False,
               help='Process the data to only output one set of data for each day')
-def main(filename, json_key, strip_duplicate_days):
+def main(strip_duplicate_days):
     repo = git.Repo("../../")
+    path = "/vaccination_county.json"
 
     # fetch all the git commits that updated the data file
     # thanks to https://stackoverflow.com/q/28803626
     revlist = (
-        (commit, (commit.tree / filename).data_stream.read())
-        for commit in repo.iter_commits(paths=filename)
+        (commit, (commit.tree / path).data_stream.read())
+        for commit in repo.iter_commits(paths=path)
     )
 
     # build up a dict of the data history by looking at each commit in the git history
@@ -36,9 +36,10 @@ def main(filename, json_key, strip_duplicate_days):
     out_data = []
     out_cols = {"runid": None}
     seen_data = defaultdict(set)  # dict with date keys holding the states we've seen for that date
-    for data_time, data_batch in data.items():
-        for state_data in data_batch[json_key]:
-            state_data["runid"] = data_batch["runid"]
+    for data_batch in data:
+        data_batch = data[data_batch]
+        for county_data in data_batch["vaccination_county_condensed_data"]:
+            county_data["runid"] = data_batch["runid"]
 
             # there's an anomaly in the data where one day has the date in the wrong format.
             # reformat it, hoping this was a one-time glitch
@@ -46,16 +47,16 @@ def main(filename, json_key, strip_duplicate_days):
                 state_data["Date"] = "2021-01-12"
 
             # have we seen this state/date before?
-            if strip_duplicate_days and state_data["ShortName"] in seen_data[state_data["Date"]]:
+            if strip_duplicate_days and county_data["FIPS"] in seen_data[county_data["Date"]]:
                 continue
 
             # keep track of all the columns that exist as we go (we'll need that list for the CSV output)
             for k in state_data.keys():
                 out_cols[k] = None
             # and keep track of the state/date pairs we've seen
-            seen_data[state_data["Date"]].add(state_data["ShortName"])
+            seen_data[county_data["Date"]].add(county_data["FIPS"])
             # and append the state's data to our output
-            out_data.append(state_data)
+            out_data.append(county_data)
 
     # output the data in CSV format
     writer = csv.DictWriter(sys.stdout, out_cols.keys())

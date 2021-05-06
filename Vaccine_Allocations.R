1297ca0e4642fd8f8297337b877bbf860d8962e8
bhrenton <78880483+bhrenton@users.noreply.github.com>
2021-05-06 00:49:52 -0400

    Update Vaccine_Allocations.R

--- Vaccine_Allocations.R
+++ Vaccine_Allocations.R
@@ -1,13 +1,13 @@
 library(tidyverse)
-pfizer <- read_csv("https://data.cdc.gov/resource/saz5-9hgg.csv") %>% 
+pfizer <- read_csv("https://data.cdc.gov/resource/saz5-9hgg.csv?$limit=50000") %>% 
   mutate(key= paste(jurisdiction, week_of_allocations)) %>% 
   rename(pfizer_1st_dose = `_1st_dose_allocations`) %>% 
   rename(pfizer_2nd_dose = `_2nd_dose_allocations`)
-moderna <- read_csv("https://data.cdc.gov/resource/b7pe-5nws.csv") %>% 
+moderna <- read_csv("https://data.cdc.gov/resource/b7pe-5nws.csv?$limit=50000") %>% 
   mutate(key= paste(jurisdiction, week_of_allocations))%>% 
   rename(moderna_1st_dose = `_1st_dose_allocations`) %>% 
   rename(moderna_2nd_dose = `_2nd_dose_allocations`)
-janssen <- read_csv("https://data.cdc.gov/resource/w9zu-fywh.csv") %>% 
+janssen <- read_csv("https://data.cdc.gov/resource/w9zu-fywh.csv?$limit=50000") %>% 
   mutate(key= paste(jurisdiction, week_of_allocations)) %>% 
   rename(janssen_1st_dose = `_1st_dose_allocations`)
 pfizer_moderna <- left_join(pfizer, moderna, by = "key")
